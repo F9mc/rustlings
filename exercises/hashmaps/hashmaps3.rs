0c2240ea6e50d872c6a48df48ac39aa78387b581
Paul <paul.cabon@protonmail.com>
2023-02-07 12:18:11 +0100

    Step 2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(PartialEq)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -40,6 +40,20 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        match scores.get(&team_1_name){
+            Some(previous) => scores.insert((*team_1_name).to_string(), Team {name:team_1_name, goals_scored:previous.goals_scored+team_1_score, goals_conceded:previous.goals_conceded+team_2_score}),
+            None => scores.insert((*team_1_name).to_string(), Team {name:team_1_name, goals_scored:team_1_score, goals_conceded:team_2_score}),
+        };
+
+        match scores.get(&team_2_name){
+            Some(previous) => scores.insert((*team_2_name).to_string(), Team {name:team_2_name, goals_scored:previous.goals_scored+team_2_score, goals_conceded:previous.goals_conceded+team_1_score}),
+            None => scores.insert((*team_2_name).to_string(), Team {name:team_2_name, goals_scored:team_2_score, goals_conceded:team_1_score}),
+        };
+
+
+
+
     }
     scores
 }
